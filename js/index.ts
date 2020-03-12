24b72a81e9a77246f355da114eb3764efc77cf99
Vitaly <vitalykuklin@gmail.com>
2020-03-12 07:33:20 +0700

    Feature: only update [ref: JA & Vue tasks]

--- js/index.ts
+++ js/index.ts
@@ -32,29 +32,64 @@
 
 // export * from "./TweenMax.min.js";
 
-const niceMenuBtn = document.querySelector("ul.menu-btn"); //* slideToggle menu
-
-let editPopupSwitcher = false;
-
-const editPopup = document.querySelector(".edit-popup");
-const chThemeBtn = document.querySelector("#edit"); //* change theme button
-chThemeBtn?.addEventListener("click", function(event) {
-  let target = event.target;
-  target.classList.remove("pulse"); //* not pulse button when first click
-  editPopupSwitcher = !editPopupSwitcher;
-  if (editPopupSwitcher) {
-    TwinMax.to(editPopup, 1, {
-      x: "2%",
-      display: "block",
-      height: "65vh",
-      ease: Back.easeOut
-    });
-  } else {
-    TwinMax.to(editPopup, 1, {
-      x: "-200%",
-      display: "block",
-      height: "0vh",
-      ease: Power3.easeOut
-    });
-  }
+var niceMenuBtn = document.querySelector(".menu-btn"); //* slideToggle menu
+console.log(niceMenuBtn);
+var menuWraper = document.querySelector(".menu-wrapper");
+niceMenuBtn.addEventListener("click", () => {
+  console.assert("yes");
+  menuWraper.classList.toggle("show");
 });
+
+// niceMenuBtn.addEventListener("click", () => {  //* TODO: i think good var: https://javascript.ru/forum/misc/45183-slidetoggle-bez-ispolzovaniya-jquery.html
+//   menuWraper.classList.toggle("collapsed");
+// });
+
+// niceMenuBtn.addEventListener("click", function() {
+//   if (menuWraper.clientHeight === 0) {
+//     // max-height of a number "larger-than-necessary" in order to be dynamic
+//     gsap.to(".menu-wrapper", 0.5, { css: { maxHeight: 500 } });
+//   } else {
+//     gsap.to(".menu-wrapper", 0.25, { css: { maxHeight: 0 } });
+//   }
+// });
+// console.log(menuWraper);
+
+// var menuWraper = gsap.from(".menu-wrapper", 1, {
+//   height: 0,
+//   paused: true,
+//   reversed: true
+// });
+// niceMenuBtn.addEventListener("click", doSlideToggle);
+// function doSlideToggle() {
+//   menuWraper.reversed() ? menuWraper.play() : menuWraper.reverse();
+// }
+
+var editPopupSwitcher = false;
+var editPopup = document.querySelector(".edit-popup");
+var chThemeBtn = document.querySelector("#edit"); //* change theme button
+chThemeBtn === null || chThemeBtn === void 0
+  ? void 0
+  : chThemeBtn.addEventListener("click", function(event) {
+      // var target = event.target;
+      // console.log(target);
+      this.classList.remove("pulse"); //* not pulse button when first click
+      editPopupSwitcher = !editPopupSwitcher;
+      if (editPopupSwitcher) {
+        gsap.to(editPopup, 1, {
+          //*! gsap => not TweenMax
+          x: "2%",
+          display: "block",
+          height: "70vh",
+          width: "50%",
+          ease: Back.easeOut
+        });
+      } else {
+        gsap.to(editPopup, 1, {
+          x: "-200%",
+          display: "block",
+          height: "0vh",
+          width: "80%",
+          ease: Power3.easeOut
+        });
+      }
+    });
