2e295cb9458ee0bb869bc01b9f50b62862fda1e4
Vitaly Kuklin <vitalykuklin@gmail.com>
2020-03-18 08:02:57 +0000

    Featurd:add tabs functional - need refactoring - look TODO's [ref:no-ref]

--- js/tabs.js
+++ js/tabs.js
@@ -1,20 +1,35 @@
+var _a, _b;
+function Tab(tabData, i) {
+    return "<li class=\"tab-link_tab\" id=\"" + i + "\"><span>" + tabData.name + "</span></li>";
+}
+function TabList(tabs) {
+    return "<ul>\n                " + tabs.map(function (el, i) { return Tab(el, i); }).join('') + "\n            </ul>";
+}
 var tabs = [
     {
         name: 'github',
-        contentLink: 'https://github.com/Mitrr',
-        contentLinkText: '@github/Mitrr',
-        contentText: 'Здесь вы можете посмотреть мои проекты'
+        contentLink: 'https://github.com/kuklinv',
+        contentLinkText: '@github/kuklinv',
+        contentText: 'my project on gitHub'
     },
     {
         name: 'email',
-        contentLink: 'https://github.com/Mitrr',
-        contentLinkText: '@github/Mitrr',
-        contentText: 'Ваши предложения'
+        contentLink: 'mailto:vitalykuklin@gmail.com',
+        contentLinkText: 'vitalykuklin@gmail.com',
+        contentText: 'mail me'
     },
     {
-        name: 'skype',
-        contentLink: 'https://github.com/Mitrr',
-        contentLinkText: '@github/Mitrr',
-        contentText: 'Связь со мной'
+        name: 'website',
+        contentLink: 'http://http://www.devcod.pro/',
+        contentLinkText: 'www.devcod.pro/',
+        contentText: 'visit me'
     },
 ];
+(_a = document.querySelector('.tabs_container')) === null || _a === void 0 ? void 0 : _a.innerHTML = TabList(tabs);
+function TabContent(tab) {
+    return "<div class=\"tabs_content_container\" style=\"display: block\">\n                <a href=\"" + tab.contentLink + "\" target=\"_blank\">" + tab.contentLinkText + "</a>   //TODO: not awesom look\n                <p>" + tab.contentText + "</p>\n</div>";
+}
+(_b = document.querySelector('.tab-link_tab')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', function () {
+    var _a;
+    (_a = document.querySelector('.tab-content_wrapper')) === null || _a === void 0 ? void 0 : _a.innerHTML = TabContent(tabs[+this.id]); //TODO: not work with all selectors (only first) and not hide information on second clik - need sweetcher
+});
--- js/tabs.ts
+++ js/tabs.ts
@@ -1,20 +1,43 @@
+function Tab(tabData,i) {
+    return `<li class="tab-link_tab" id="${i}"><span>${tabData.name}</span></li>`;
+}
+function TabList(tabs){
+    return `<ul>
+                ${tabs.map((el,i)=>Tab(el,i)).join('')}
+            </ul>`
+
+}
+
 let tabs = [
     {
         name:'github',
-        contentLink:'https://github.com/Mitrr',
-        contentLinkText:'@github/Mitrr',
-        contentText:'Здесь вы можете посмотреть мои проекты'
+        contentLink:'https://github.com/kuklinv',
+        contentLinkText:'@github/kuklinv',
+        contentText:'my project on gitHub'
     },
     {
         name:'email',
-        contentLink:'https://github.com/Mitrr',
-        contentLinkText:'@github/Mitrr',
-        contentText:'Ваши предложения'
+        contentLink:'mailto:vitalykuklin@gmail.com',
+        contentLinkText:'vitalykuklin@gmail.com',
+        contentText:'mail me'
     },
     {
-        name:'skype',
-        contentLink:'https://github.com/Mitrr',
-        contentLinkText:'@github/Mitrr',
-        contentText:'Связь со мной'
+        name:'website',
+        contentLink:'http://http://www.devcod.pro/',
+        contentLinkText:'www.devcod.pro/',
+        contentText:'visit me'
     },
 ];
+
+document.querySelector('.tabs_container')?.innerHTML = TabList(tabs);
+
+function TabContent(tab) {
+    return `<div class="tabs_content_container" style="display: block">
+                <a href="${tab.contentLink}" target="_blank">${tab.contentLinkText}</a>   //TODO: not awesom look
+                <p>${tab.contentText}</p>
+</div>`
+}
+
+document.querySelector('.tab-link_tab')?.addEventListener('click',function () {
+   document.querySelector('.tab-content_wrapper')?.innerHTML = TabContent(tabs[+this.id]);  //TODO: not work with all selectors (only first) and not hide information on second clik - need sweetcher
+});
