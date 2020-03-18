a5bfc61762f6470890790bea081bdea106ae3b77
Vitaly Kuklin <vitalykuklin@gmail.com>
2020-03-18 05:01:47 +0000

    Feature: add tabs section/not finished at moment[ref:no-ref]

---
+++ js/tabs.js
@@ -0,0 +1,20 @@
+var tabs = [
+    {
+        name: 'github',
+        contentLink: 'https://github.com/Mitrr',
+        contentLinkText: '@github/Mitrr',
+        contentText: 'Здесь вы можете посмотреть мои проекты'
+    },
+    {
+        name: 'email',
+        contentLink: 'https://github.com/Mitrr',
+        contentLinkText: '@github/Mitrr',
+        contentText: 'Ваши предложения'
+    },
+    {
+        name: 'skype',
+        contentLink: 'https://github.com/Mitrr',
+        contentLinkText: '@github/Mitrr',
+        contentText: 'Связь со мной'
+    },
+];
