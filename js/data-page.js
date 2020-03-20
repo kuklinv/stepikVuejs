7611aed46c7450acd32c4d348ecb35f7155533b5
Vitaly <vitalykuklin@gmail.com>
2020-03-20 07:47:41 +0700

    Feature: recompile files in es2015 + little change index and data-page [ref:no-ref]

--- js/data-page.js
+++ js/data-page.js
@@ -1,4 +1,2 @@
-"use strict";
-exports.__esModule = true;
-var theme_js_1 = require("./theme.js");
-theme_js_1.themeStorage.update();
+import { themeStorage } from './theme.js';
+themeStorage.update();
