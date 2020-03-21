9862e6525b98b249e8fe7c3bfd4810186a0e708a
Vitaly <vitalykuklin@gmail.com>
2020-03-21 15:46:32 +0700

    Feature: solved issue [ref:#9]

--- js/data-page.js
+++ js/data-page.js
@@ -1,2 +1,6 @@
 import { themeStorage } from './theme.js';
 themeStorage.moduleUpdate();
+// if its bee a _blank page - its listener need to reload all page with changed theme
+// window.addEventListener('storage',function (event) {
+//     themeStorage.moduleUpdate();
+// };
--- js/data-page.ts
+++ js/data-page.ts
@@ -1,3 +1,8 @@
 import {themeStorage} from './theme.js';
 
     themeStorage.moduleUpdate();
+
+    // if its bee a _blank page - its listener need to reload all page with changed theme
+// window.addEventListener('storage',function (event) {
+//     themeStorage.moduleUpdate();
+// };
