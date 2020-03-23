e26564d2567af0c06d3bec699b196f2b6f6f9917
Vitaly <vitalykuklin@gmail.com>
2020-03-23 07:40:16 +0700

    Problem: not finde footer-component[ref:#no-ref]

--- js/index.ts
+++ js/index.ts
@@ -1,4 +1,7 @@
 import {themeStorage} from './theme.js';
+import {FooterComponent} from './footer-component';
+
+new FooterComponent(document.querySelector('.footer-component'));
 
 let allContentWindow = document.querySelector(".content-wrapper"); // window
 let niceMenuBtn = document.querySelector(".menu-btn"); //* slideToggle menu !!//
@@ -101,24 +104,6 @@ previewRadioGroup?.addEventListener('click', (event) => {
     currentTheme.secondaryColor = target.style.color;
     return;
 });
-
-// let themeStorage = {
-//     themeType: 'indigo-theme',
-//     secondaryColor: '#4ebdde4',
-//     update: function () {
-//         document.querySelector("#edit")?.style.backgroundColor = this.secondaryColor;
-//         document.querySelector(".content-wrapper")?.classList.value = `content-wrapper ${this.themeType}`;
-//         document.querySelector(".nav")?.classList.value = `nav-wrapper navbar-fixed indigo-theme ${this.themeType}`;
-//         let menuItem = [...document.querySelector('.menu-btn')?.childNodes];
-//         menuItem.forEach((el) => {
-//             if (el.nodeType !== 3) {
-//                 el.style.backgroundColor = this.secondaryColor;
-//             }
-//         });
-//         localStorage.setItem('themeType', this.themeType);
-//         localStorage.setItem('secondaryColor', this.secondaryColor);
-//     }
-// };
 
 document.addEventListener('DOMContentLoaded', function () { // for save theme betwean reloads page - -use localStorage!!!!!!!!!!
     let sColor = localStorage.getItem('secondaryColor');   // its result !!!!!! main result
