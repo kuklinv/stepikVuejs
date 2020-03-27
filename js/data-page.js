a0fe5cd7305cf9f5f14f6f793055cb1997a45fb1
kuklinv <vitalykuklin@gmail.com>
2020-03-27 13:26:00 +0700

    Feature: render planets on data-page from swapi [ref:#17]

--- js/data-page.js
+++ js/data-page.js
@@ -27,30 +27,50 @@ toolTipElement.addEventListener('mouseleave', () => {
     toolTipElement.style.display = 'none';
 });
 //rest api from swapi.io
-const planet = {
-    name: "Alderaan",
-    rotation_period: "24",
-    orbital_period: "364",
-    diameter: "12500",
-    climate: "temperate",
-    gravity: "1 standard",
-    terrain: "grasslands, mountains",
-    surface_water: "40",
-    population: "2000000000",
-    residents: [
-        "https://swapi.co/api/people/5/",
-        "https://swapi.co/api/people/68/",
-        "https://swapi.co/api/people/81/"
-    ]
-};
+// const planet = {
+//     name: "Alderaan",
+//     rotation_period: "24",
+//     orbital_period: "364",
+//     diameter: "12500",
+//     climate: "temperate",
+//     gravity: "1 standard",
+//     terrain: "grasslands, mountains",
+//     surface_water: "40",
+//     population: "2000000000",
+//     residents: [
+//         "https://swapi.co/api/people/5/",
+//         "https://swapi.co/api/people/68/",
+//         "https://swapi.co/api/people/81/"
+//     ]
+// };
 // fetch('https://swapi.co/api/planets/').then(res => res.json(res)).then(data => console.log(data));
+let planetArr = []; // all planets on api page (10)
+let pageChunckPlanets = []; // 5 planets on our page
+let nextPlanetPage = ''; // next api page
+let prevPlanetPage = ''; // prev api page
 function renderPlanet(planet) {
-    return `<div class="personage-item_container" id="planet_${planet.id}" style="flex: 1;transform: translateY(70%)">
-     <h5 style="margin: 0 auto">planet:${planet.name}</h5>
-     <p>gravity:${planet.gravity}</p>
-     <p>terrain:${planet.terrain}</p>
-     <p>diameter:${planet.diameter}</p>
-     <p>population:${planet.population}</p>
-     <p>residents:${planet.residents[0], planet.residents[1], planet.residents[2]}</p>
+    return `<div class="personage-item_container" id="planet_${planet.index}" style="flex: 1;transform: translateY(50%)">
+     <h5 style="margin: 0 auto">planet: ${planet.name}</h5>
+     <p>gravity: ${planet.gravity}</p>
+     <p>terrain: ${planet.terrain}</p>
+     <p>diameter: ${planet.diameter}</p>
+     <p>population: ${planet.population}</p>
+     <p>residents: ${planet.residents[0], planet.residents[1], planet.residents[2]}</p>
      </div>`;
 }
+function drawPlanetsOnHTML(startIndex = 0, endIndex = 5) {
+    pageChunckPlanets = planetArr.slice(startIndex, endIndex);
+    let drawPlanets = pageChunckPlanets.map(item => renderPlanet(item)); // draw on html 5-chunk planets
+    let planetContainer = document.querySelector('.personages_container');
+    planetContainer.innerHTML = drawPlanets.join('');
+}
+window.onload = async function () {
+    let res = await fetch('https://swapi.co/api/planets/').then(res => res.json());
+    planetArr = res.results.map((el, index) => {
+        el.index = index;
+        return el;
+    });
+    nextPlanetPage = res.next; // next page from api (10 items on one page)
+    prevPlanetPage = res.previous;
+    drawPlanetsOnHTML();
+};
