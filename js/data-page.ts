a4dfbc083e49b9f8f3a3e3ecda5ce19cb718d05f
kuklinv <vitalykuklin@gmail.com>
2020-03-27 11:32:56 +0700

    Feature: render planets on data-page in progress [ref:#17]

--- js/data-page.ts
+++ js/data-page.ts
@@ -34,3 +34,36 @@ toolTipElement.addEventListener('mouseover', () => {
 toolTipElement.addEventListener('mouseleave', () => {
     toolTipElement.style.display = 'none';
 });
+
+//rest api from swapi.io
+
+const planet = {
+    name: "Alderaan",
+    rotation_period: "24",
+    orbital_period: "364",
+    diameter: "12500",
+    climate: "temperate",
+    gravity: "1 standard",
+    terrain: "grasslands, mountains",
+    surface_water: "40",
+    population: "2000000000",
+    residents: [
+        "https://swapi.co/api/people/5/",
+        "https://swapi.co/api/people/68/",
+        "https://swapi.co/api/people/81/"
+    ]
+};
+
+// fetch('https://swapi.co/api/planets/').then(res => res.json(res)).then(data => console.log(data));
+
+function renderPlanet(planet) {
+    return `<div class="personage-item_container" id="planet_${planet.id}" style="flex: 1;transform: translateY(70%)">
+     <h5 style="margin: 0 auto">planet:${planet.name}</h5>
+     <p>gravity:${planet.gravity}</p>
+     <p>terrain:${planet.terrain}</p>
+     <p>diameter:${planet.diameter}</p>
+     <p>population:${planet.population}</p>
+     <p>residents:${planet.residents[0], planet.residents[1], planet.residents[2]}</p>
+     </div>`
+}
+
