d433b2d649b2e5602861159e9de7d551674fbee2
kuklinv <vitalykuklin@gmail.com>
2020-03-29 23:35:56 +0700

    Feature: continue with vue/not worked mounted() [ref:#22]

--- js/search.js
+++ js/search.js
@@ -1,14 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const initSearch = 'what you search?';
 let vm = new Vue({
     el: '#search',
     data: {
         header: "Hello Vue.js developer",
-        request: initSearch
+        request: initSearch,
+        films: []
     },
     computed: {
         searching() {
-            return this.request === '' || this.request === initSearch ? 'nothing jet' : `search:${this.request}`;
+            return this.request === '' || this.request === initSearch ? 'nothing jet    ' : `search:${this.request}`;
         }
+    },
+    mounted() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.films = yield fetch('http https://swapi.co/api/films/').then(res => res.json()).then(data => data.results);
+        });
     }
 });
--- ts-src/search.ts
+++ ts-src/search.ts
@@ -4,11 +4,15 @@ let vm = new Vue({
     el: '#search',
     data: {
         header: "Hello Vue.js developer",
-        request: initSearch
+        request: initSearch,
+        films: []
     },
     computed: {
         searching() {
-            return this.request === '' || this.request === initSearch ? 'nothing jet' : `search:${this.request}`;
+            return this.request === '' || this.request === initSearch ? 'nothing jet    ' : `search:${this.request}`;
         }
+    },
+    async mounted() {
+        this.films = await fetch('http https://swapi.co/api/films/').then(res => res.json()).then(data => data.results);
     }
 });
